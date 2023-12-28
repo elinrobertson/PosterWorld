b4e586f2e6d5072817f5ba4221014924e19a8c9d
elinrobertson <114143751+elinrobertson@users.noreply.github.com>
2023-12-28 22:28:30 +0100

    updated product.model

--- server/src/resources/product/product.model.js
+++ server/src/resources/product/product.model.js
@@ -5,9 +5,9 @@ const ProductSchema = new Schema({
     title: {type: String, required: true},
     price: {type: Number, required: true},
     category: { type: Schema.Types.ObjectId, ref: "category", required: true },
-    inStock: {type: Number, required: true, default: 0},
-    image: {type: String, required: true},
     description: {type: String, required: true},
+    inStock: {type: Number, required: true, default: 0},
+    images: {type: String, required: true},
 },{versionKey: false});
 
 const ProductModel = models.product || model("product", ProductSchema);
@@ -16,10 +16,10 @@ const productJoiSchema = Joi.object({
     title: Joi.string().strict().required(),
     price: Joi.number().strict().required(),
     category: Joi.string().required(),
+    description: Joi.string().strict().required(),
     // category: Joi.objectId().required(), osäker på vilken som ska vara här
     inStock: Joi.number().strict().required(),
-    image: Joi.string().uri().required(),
-    description: Joi.string().strict().required(),
+    images: Joi.string().uri().required(),
 });
 
-module.exports = { ProductModel, ProductSchema, productJoiSchema }
+module.exports = { ProductModel, ProductSchema }
