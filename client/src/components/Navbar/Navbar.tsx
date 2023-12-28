62c5c1ff4eda6582bfe1b07c0e06ae80307d5272
elinrobertson <114143751+elinrobertson@users.noreply.github.com>
2023-12-28 13:09:23 +0100

    made some light styling to the navbar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { GoPerson } from "react-icons/go";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+        <div className="discount-container">
+            <p>Här kommer lite random text om en rabattkod</p>
+        </div>
+        <div className="navbar-container">
+            <div className="logo-container">
+                <h2>Poster</h2><span>World</span>
+            </div>
+            <div className="links-container">
+                <ul>
+                    <li>Natur</li>
+                    <li>Djur</li>
+                    <li>Botanisk</li>
+                    <li>Text</li>
+                    <li>Kök</li>
+                    <li>Städer</li>
+                </ul>
+            </div>
+            <div className="icons-container">
+                <GoPerson />
+                <HiOutlineShoppingBag />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
