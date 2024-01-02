b5591ef80b121dfe50ab5bcb00f1b9204af1ab49
elinrobertson <114143751+elinrobertson@users.noreply.github.com>
2024-01-02 18:42:03 +0100

    created functions for user

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const cookieSession = require("cookie-session");
 const productRouter = require("./resources/product/product.router");
 const categoryRouter = require("./resources/category/category.router");
+const { userRouter } = require("./resources/user/user.router");
 
 const app = express();
 
@@ -13,6 +15,17 @@ app.use(
     })
 );
 
+app.use(
+    cookieSession({
+      name: "session",
+      keys: ["aVeryS3cr3tK3y"],
+      maxAge: 1000 * 60 * 60 * 24, // 24 Hours
+      sameSite: "strict",
+      httpOnly: true,
+      secure: false,
+    })
+  );
+
 // Loggningsmiddleware för att visa detaljerade felmeddelanden
 app.use((err, req, res, next) => {
     console.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
@@ -22,5 +35,6 @@ app.use((err, req, res, next) => {
 // Routers
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoryRouter);
+app.use("/api/users", userRouter)
 
 module.exports = { app };
---
+++ server/src/resources/user/user.router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { registerUser, loginUser, logoutUser } = require("./user.controller");
+const { userJoiSchema } = require("./user.model");
+const { validate } = require("../../middlewares");
+const userRouter = express.Router();
+
+userRouter.post("/register", validate(userJoiSchema), registerUser);
+userRouter.post("/login", loginUser)
+userRouter.post("/logout", logoutUser)
+
+module.exports = { userRouter }
