73db3facf2dc2e2b7731a01177378e9fa42952ee
elinrobertson <114143751+elinrobertson@users.noreply.github.com>
2024-01-12 11:07:41 +0100

    added fonctionality for in stock

--- client/src/components/ProductDetail/ProductDetail.tsx
+++ client/src/components/ProductDetail/ProductDetail.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { IoMdCheckmarkCircleOutline } from 'react-icons/io';
+import { IoCloseOutline } from "react-icons/io5";
 import { useCart } from '../../context/CartContext';
 import "./ProductDetail.css";
 
-interface ProductDetailProps {
-
-}
+interface ProductDetailProps {}
 
 const ProductDetail: React.FC<ProductDetailProps> = () => {
   const { id } = useParams();
@@ -37,44 +37,60 @@ const ProductDetail: React.FC<ProductDetailProps> = () => {
 
   const scroll = (direction: 'left' | 'right') => {
     if (direction === 'left') {
-        setCurrentSlide((prevSlide) => (prevSlide === 0 ? product.images.length - 1 : prevSlide - 1));
+      setCurrentSlide((prevSlide) => (prevSlide === 0 ? product.images.length - 1 : prevSlide - 1));
     } else {
-        setCurrentSlide((prevSlide) => (prevSlide === product.images.length - 1 ? 0 : prevSlide + 1));
+      setCurrentSlide((prevSlide) => (prevSlide === product.images.length - 1 ? 0 : prevSlide + 1));
     }
-};
+  };
 
-const handleAddToCart = () => {
-  if(product) {
-    addToCart(product._id);
-  }
-}
+  const handleAddToCart = (stock: number) => {
+    if (stock > 0) {
+      addToCart(product._id);
+    }
+  };
 
   return (
     <div className="productdetail-wrapper">
       {product ? (
         <>
-        <div className="image-container">
-          <img className="product-img" src={product.images[currentSlide]} alt={product.title} />
+          <div className="image-container">
+            <img className="product-img" src={product.images[currentSlide]} alt={product.title} />
             {product.images.length > 1 && (
               <div className="arrow-icons">
                 <MdOutlineKeyboardArrowLeft className="arrows" onClick={() => scroll('left')} />
                 <MdOutlineKeyboardArrowRight className="arrows" onClick={() => scroll('right')} />
               </div>
             )}
-        </div>
-        <div className="detail-container">
-          <h2>{product.title}</h2>
-          <p id='description'>{product.description}</p>
-          <p id='price'>{product.price} kr</p>
-          <div className="button-container">
-            <motion.button 
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              onClick={handleAddToCart}>Lägg i varukorgen
-            </motion.button>
-            <p id='instock'>In Stock: {product.inStock}</p>
           </div>
-        </div>
+          <div className="detail-container">
+            <h2>{product.title}</h2>
+            <p id='description'>{product.description}</p>
+            <p id='price'>{product.price} kr</p>
+            <div className="button-container">
+              <motion.button
+                whileHover={product.inStock > 0 ? { scale: 1.1 } : {}}
+                whileTap={product.inStock > 0 ? { scale: 0.9 } : {}}
+                onClick={() => handleAddToCart(product.inStock)}
+                disabled={product.inStock === 0}
+                className={product.inStock === 0 ? "motion-button-disabled" : ""}
+              >
+                Lägg i varukorgen
+              </motion.button>
+              <p id='instock'>
+                {product.inStock > 0 ? (
+                  <>
+                    I lager
+                    <IoMdCheckmarkCircleOutline style={{ marginLeft: '5px' }} />
+                  </>
+                ) : (
+                  <>
+                  <p style={{ color: 'rgb(219, 107, 95)' }}>Ej i lager</p>
+                  <IoCloseOutline style={{ color: 'rgb(219, 107, 95)', marginLeft: '5px' }} />
+                </>
+                )}
+              </p>
+            </div>
+          </div>
         </>
       ) : (
         <p>Loading...</p>
