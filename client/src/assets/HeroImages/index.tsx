4706a201dee5b861cb56057da53140c717294ac5
elinrobertson <114143751+elinrobertson@users.noreply.github.com>
2024-01-15 08:57:50 +0100

    small changes

--- client/src/assets/HeroImages/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as hero1 } from "./hero1.jpg"
-export { default as hero2 } from "./hero2.jpg"
-export { default as hero3 } from "./hero3.jpg"
--- server/src/middlewares.js
+++
@@ -1,9 +0,0 @@
-function validate (schema) {
-    return function (req, res, next) {
-        const { error } = schema.validate(req.body);
-        if (!error) return next();
-        res.status(400).json(error.message);
-    };
-}
-
-module.exports = { validate };
