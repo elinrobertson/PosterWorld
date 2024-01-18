d22eb68a308bd3be0e9b5ca63762eab6fea60495
elinrobertson <114143751+elinrobertson@users.noreply.github.com>
2024-01-18 12:02:45 +0100

    small changes

--- client/src/components/CategoryPage/CategoryPage.tsx
+++ client/src/components/CategoryPage/CategoryPage.tsx
@@ -1,49 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import ProductList from "../ProductList/ProductList";
 
-interface Category {
-  _id: string;
-  title: string;
-  description: string;
-}
 
 const CategoryPage: React.FC = () => {
-  const { category } = useParams<{ category?: string }>();
-  const [categoryData, setCategoryData] = useState<Category | null>(null);
+    const { category } = useParams<{ category: string }>();
 
-  useEffect(() => {
-    const fetchCategoryData = async () => {
-      try {
-        if (category) {
-          const response = await fetch(`http://localhost:3000/api/categories/${category.toLowerCase()}`);
+    return (
+        <div>
+            <ProductList categoryName={category || ''} />
+        </div>
+    )
+}
 
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-
-          const data = await response.json();
-          setCategoryData(data);
-        }
-      } catch (error) {
-        console.error('Error fetching category data:', error);
-      }
-    };
-
-    fetchCategoryData();
-  }, [category]);
-
-  return (
-    <div>
-      {categoryData && (
-        <>
-          <h2>{categoryData.title}</h2>
-          <p>{categoryData.description}</p>
-          <ProductList categoryData={categoryData} />
-        </>
-      )}
-    </div>
-  );
-};
 
 export default CategoryPage;
